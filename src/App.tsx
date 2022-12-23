439dfd48d1a1c709fd48783f238cbeebd9fd4863
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-12-24 02:08:23 +0300

    feat(app): feat RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-
 import { Book, Header } from "components";
+import { RouterProvider } from "react-router-dom";
+import { router } from "router/router";
 import { AppWrapper } from "ui";
 
-
-
 export const App = () => {
-  
   return (
     <AppWrapper>
+      <RouterProvider router={router} />
       <Header />
       <Book />
-
-     
     </AppWrapper>
   );
 };
