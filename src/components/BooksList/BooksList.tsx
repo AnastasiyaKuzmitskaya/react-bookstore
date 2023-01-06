582f42fb35629778e04d810984f4681db21df445
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2023-01-06 13:12:46 +0300

    fix(components): fix bookList spinners

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,27 +1,34 @@
+import { CSSProperties } from "react";
 import { INewBookApi } from "services";
 import { BookItem } from "components";
 import { ErrorMassage, StyledBookList } from "./styles";
-import ColorRing from "react-spinners/CircleLoader";
+import RotatingLines from "react-spinners/RotateLoader";
+import { Color } from "ui";
 interface IProps {
   books: INewBookApi[];
   isLoading: boolean;
   error: string | null;
 }
+const override: CSSProperties = {
+  display: "block",
+  margin: "100px auto",
+};
 
 export const BooksList = ({ books, isLoading, error }: IProps) => {
   if (isLoading) {
-    return <ColorRing loading={isLoading} size={100} />;
+    return (
+      <RotatingLines cssOverride={override} loading={isLoading} color={Color.Primary} size={100} />
+    );
   }
 
   if (error) {
-    return <ErrorMassage>Sorry, {error}</ErrorMassage>;
+    return <ErrorMassage>Sorry, an error has occurred. Try again</ErrorMassage>;
   }
 
   return (
     <>
       {" "}
       <StyledBookList>
-        {error && <p />}
         {books.map((book) => (
           <BookItem book={book} key={book.isbn13} />
         ))}
