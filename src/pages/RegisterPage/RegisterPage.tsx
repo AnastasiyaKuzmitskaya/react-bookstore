431ea54c3e7f5db6135dd61eb2b6eb490106c59f
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2023-01-16 04:35:42 +0300

    feat(page): feat registerPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { SignInForm, SignUpForm } from "components";
+import { useState } from "react";
+import { FormTab, FormWrapper, StyledRegisterPage, SwitchWrapper } from "./styles";
 
+export interface IFormSwitch {
+  signIn: boolean;
+  signUp: boolean;
+}
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [activeTab, setActiveTab] = useState<IFormSwitch>({
+    signIn: true,
+    signUp: false,
+  });
+
+  const handleSignIn = () => {
+    setActiveTab({
+      signIn: true,
+      signUp: false,
+    });
+  };
+
+  const handleSignUp = () => {
+    setActiveTab({
+      signIn: false,
+      signUp: true,
+    });
+  };
+
+  return (
+    <StyledRegisterPage>
+      <FormWrapper>
+        <SwitchWrapper>
+          <FormTab isActive={activeTab.signIn} onClick={handleSignIn}>
+            Sign in
+          </FormTab>
+
+          <FormTab isActive={activeTab.signUp} onClick={handleSignUp}>
+            Sign Up
+          </FormTab>
+        </SwitchWrapper>
+        {activeTab.signIn && <SignInForm />}
+        {activeTab.signUp && <SignUpForm />}
+      </FormWrapper>
+    </StyledRegisterPage>
+  );
 };
---
+++ src/pages/RegisterPage/styles.ts
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { Color, Media, Typography } from "ui";
+
+interface ITabProps {
+  isActive: boolean;
+}
+
+export const StyledRegisterPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 500px;
+  padding: 0px 32px 40px;
+
+  border: 1px solid ${Color.Gray};
+
+  ${Media.SM} {
+    width: 300px;
+    padding: 0px 20px 30px;
+  }
+`;
+
+export const SwitchWrapper = styled.div`
+  display: flex;
+
+  width: 100%;
+  height: 72px;
+
+  ${Media.SM} {
+    height: 48px;
+  }
+`;
+
+export const FormTab = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 50%;
+
+  ${Typography.S1};
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+
+  border-bottom: ${({ isActive }: ITabProps) =>
+    isActive ? `2px solid ${Color.Primary}` : `2px solid ${Color.Lighte}`};
+  border-color: ${({ isActive }: ITabProps) =>
+    isActive ? `2px solid ${Color.Primary}` : `2px solid ${Color.Lighte}`};
+  color: ${Color.Primary};
+
+  cursor: pointer;
+
+  ${Media.SM} {
+    font-size: 18px;
+  }
+`;
